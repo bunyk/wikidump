88ebcc9deea089c4dcc5d6de74f359b0dd5ac604
Taras Bunyk <tbunyk@gmail.com>
2018-12-08 00:26:17 +0200

    tweaks

--- remove_spam.py
+++ remove_spam.py
@@ -1,4 +1,6 @@
 import csv
+import re
+import random
 
 import mwparserfromhell
 import pywikibot
@@ -7,8 +9,12 @@ from diff_match_patch import diff_match_patch
 
 def main():
     domains = list(read_domains_list('spamsites.csv'))
+    random.shuffle(domains)
     for page in pages_linking_to(domains):
+        print()
+        print('=' * 50)
         print(page.title())
+        print('-' * 50)
         new_text = page.text
         new_text = ref_cleaner(new_text, domains)
         new_text = external_links_cleaner(new_text, domains)
@@ -17,7 +23,7 @@ def main():
 
 def show_domains_lines(text, domains):
     for line in text.splitlines():
-        if text_has_domains(line, domains):
+        if text_has_domains(line, domains, pattern='%s'):
             print(line)
 
 def ref_cleaner(text, domains):
@@ -29,30 +35,33 @@ def ref_cleaner(text, domains):
             wikicode.replace(tag, '{{fact}}')
 
     res = str(wikicode)
-    res = res.replace('{{fact}}<ref', '<ref') # TODO: replace to regexp and add \s* between template and ref
-    res = res.replace('ref>{{fact}}', 'ref>')
+    res = re.sub(r'{{fact}}\s*<ref', '<ref', res)
+    res = re.sub(r'ref>\s*{{fact}}', 'ref>', res)
+    res = re.sub(r'{{fact}}\s*{{Неавторитетне джерело}}', '{{fact}}', res)
+    res = re.sub(r'{{Неавторитетне джерело}}\s*{{fact}}', '{{fact}}', res)
+    res = re.sub(r'(<ref\s+name=.+?\s*/>\s*){{fact}}', r'\1', res)
     return res
 
 def external_links_cleaner(text, domains):
     res = []
     for line in text.splitlines():
         if text_has_domains(line, domains):
-            if line.startswith('* {{cite'):
+            if re.match('^\*\s*\[?http', line):
                 continue
-            if line.startswith('* [http'):
-                continue
-            if line.startswith('* http'):
+            if re.match('^\*\s*\{\{cite', line):
                 continue
         res.append(line)
     return '\n'.join(res)
 
-def text_has_domains(text, domains):
-    return any(domain in text for domain in domains)
+def text_has_domains(text, domains, pattern='/%s/'):
+    for domain in domains:
+        print(pattern % domain)
+    return any((pattern % domain) in text for domain in domains)
 
 def pages_linking_to(domains):
     for domain in domains:
         for page in pagegenerators.SearchPageGenerator(
-            'insource:/%s/' % domain.replace('.', r'\.'),
+            r'insource:/%s/' % domain.replace('.', r'\.'),
             namespaces=[0],
         ):
             yield page
@@ -68,30 +77,34 @@ def update_page(page, new_text, description, yes=False):
             print('Нічого не міняли')
             return
 
-        dmp = diff_match_patch()
-        diff = dmp.diff_main(page.text, new_text)
-        dmp.diff_cleanupSemantic(diff)
-        short_diff = []
-        for action, text in diff:
-            if action == 0 and len(text) > 210:
-                text = text[:100] + ' ... ' + text[-100:]
-            short_diff.append((action, text))
+        #  dmp = diff_match_patch()
+        #  diff = dmp.diff_main(page.text, new_text)
+        #  dmp.diff_cleanupSemantic(diff)
+        #  short_diff = []
+        #  for action, text in diff:
+        #      if action == 0 and len(text) > 210:
+        #          text = text[:100] + ' ... ' + text[-100:]
+        #      short_diff.append((action, text))
+        #  patches = dmp.patch_make(page.text, new_text)
+        #  diff = dmp.patch_toText(patches)
+        #  print(diff)
 
-        html = dmp.diff_prettyHtml(short_diff)
-        with open('diff.html', 'w') as f:
-            f.write(f'''<!DOCTYPE html><html>
-                <head>
-                    <meta charset=utf-8>
-                    <title>Simple HTML5</title>
-                </head>
+        # html = dmp.diff_prettyHtml(short_diff)
+        # with open('diff.html', 'w') as f:
+        #     f.write(f'''<!DOCTYPE html><html>
+        #         <head>
+        #             <meta charset=utf-8>
+        #             <title>Simple HTML5</title>
+        #         </head>
 
-                <body>
-                {html}
-                </body>
-                </html>
-            ''')
-        print(diff)
-        # pywikibot.showDiff(page.text, new_text)
+        #         <body>
+        #         {html}
+        #         </body>
+        #         </html>
+        #     ''')
+        # print(diff)
+
+        pywikibot.showDiff(page.text, new_text)
 
         print(description)
 
